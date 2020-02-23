e62e2c0ededdfcd0615134e1e0e0a49624d96797
MalenaLucero <luceromalena@hotmail.com>
2020-02-23 18:26:53 -0300

    first row grey table added

--- main.js
+++ main.js
@@ -7,34 +7,55 @@ const convert = () =>{
     rows = document.getElementById("rows").value
     columns = document.getElementById("columns").value
     if(input !== ''){
+        dataCleaner()
         elements = getElements(input)
         createTable()
+        createAlternativeTable()
+        createFirstRowGreyTable()
     }
     console.log(elements)
+}
+
+const dataCleaner = () =>{
+    innerHTMLCleaner('result')
+    innerHTMLCleaner('HTMLcode')
+    innerHTMLCleaner('alternativeResult')
+    innerHTMLCleaner('alternativeHTMLcode')
+    innerHTMLCleaner('firstRowGreyResult')
+    innerHTMLCleaner('firstRowGreyHTMLcode')
+    innerHTMLCleaner('standardTablePreviewTitle')
+    innerHTMLCleaner('standardTableCodeTitle')
+    innerHTMLCleaner('alternativeTablePreviewTitle')
+    innerHTMLCleaner('alternativeTableCodeTitle')
+    innerHTMLCleaner('firstRowGreyPreviewTitle')
+    innerHTMLCleaner('firstRowGreyCodeTitle')
 }
 
 const getElements = (inputValue) =>{
     const toHTML = document.createElement('div')
     toHTML.innerText = inputValue
     const HTMLstring = toHTML.innerHTML
-    return HTMLstring.split("<br><br>")
-                        .filter(e=>e!=='')
+    let arrayOfElements = HTMLstring.split("<br><br>")
                         .map(e=>{
                             if(e.includes('<br>')){
                                 return e.replace('<br>', '')
                             }else{
                                 return e
                             }
-                        }).map(e=>e.trim())
+                        })
+                        .map(e=>e.trim())
+    arrayOfElements.pop()
+    return arrayOfElements
+
 }
 
 const createTable = () =>{
+    printTitle('h2', 'Preview: Standard table (with fixed width and margin-left)', 'standardTablePreviewTitle')
     let index = 0
     const table = document.createElement('table')
     table.cellSpacing = "0"
     table.cellPadding = "0"
     table.border = "1"
-    //table.style.marginLeft = "20px"
     const tbody = document.createElement('tbody')
     tbody.border = "1"
     const specificationsTr = document.createElement('tr')
@@ -58,6 +79,7 @@ const createTable = () =>{
             const p = document.createElement('p')
             p.innerText = elements[index]
             p.style.margin = 0
+            
             index = index + 1
             td.appendChild(p)
             tr.appendChild(td)
@@ -67,6 +89,113 @@ const createTable = () =>{
     table.appendChild(tbody)
     const p = document.getElementById('result')
     p.appendChild(table)
+    printTitle('h3', 'Standard table HTML code', 'standardTableCodeTitle')
     const codeContainer = document.getElementById('HTMLcode')
     codeContainer.innerText = p.innerHTML
+}
+
+const createAlternativeTable = () =>{
+    printTitle('h2', 'Preview: Alternative table (with dynamic internal width and without margin-left)', 'alternativeTablePreviewTitle')
+    let index = 0
+    const table = document.createElement('table')
+    table.width = '550px'
+    table.cellSpacing = "0"
+    table.cellPadding = "0"
+    table.border = "1"
+    const tbody = document.createElement('tbody')
+    tbody.border = "1"
+    const specificationsTr = document.createElement('tr')
+    const specificationsTd = document.createElement('td')
+    specificationsTd.colSpan = `${columns}`
+    specificationsTd.style = "background-color: #cd2e2a; border: 1px solid black"
+    specificationsTd.width = "550px"
+    const specificationsP = document.createElement('p')
+    specificationsP.innerText = 'Specifications'
+    specificationsP.style = "color: white !important; margin: 3px"
+    specificationsTd.appendChild(specificationsP)
+    specificationsTr.appendChild(specificationsTd)
+    tbody.appendChild(specificationsTr)
+    for(let i=0; i < rows; i++){
+        const tr = document.createElement('tr')
+        tr.border = "1"
+        for(let j=0; j < columns; j++){
+            const td = document.createElement('td')
+            td.style = "border: 1.0px solid black"
+            const p = document.createElement('p')
+            p.innerText = elements[index]
+            p.style.margin = '3px'
+            index = index + 1
+            td.appendChild(p)
+            tr.appendChild(td)
+        }
+        tbody.appendChild(tr)
+    }
+    table.appendChild(tbody)
+    const p = document.getElementById('alternativeResult')
+    p.appendChild(table)
+    printTitle('h3', 'Alternative table HTML code', 'alternativeTableCodeTitle')
+    const codeContainer = document.getElementById('alternativeHTMLcode')
+    codeContainer.innerText = p.innerHTML
+}
+
+const createFirstRowGreyTable = () =>{
+    printTitle('h2', 'Preview: Alternative table with first row grey', 'firstRowGreyPreviewTitle')
+    let index = 0
+    const table = document.createElement('table')
+    table.width = '550px'
+    table.cellSpacing = "0"
+    table.cellPadding = "0"
+    table.border = "1"
+    const tbody = document.createElement('tbody')
+    tbody.border = "1"
+    const specificationsTr = document.createElement('tr')
+    const specificationsTd = document.createElement('td')
+    specificationsTd.colSpan = `${columns}`
+    specificationsTd.style = "background-color: #cd2e2a; border: 1px solid black"
+    specificationsTd.width = "550px"
+    const specificationsP = document.createElement('p')
+    specificationsP.innerText = 'Specifications'
+    specificationsP.style = "color: white !important; margin: 3px"
+    specificationsTd.appendChild(specificationsP)
+    specificationsTr.appendChild(specificationsTd)
+    tbody.appendChild(specificationsTr)
+    for(let i=0; i < rows; i++){
+        const tr = document.createElement('tr')
+        tr.border = "1"
+        for(let j=0; j < columns; j++){
+            const td = document.createElement('td')
+            td.style = "border: 1.0px solid black"
+            td.style.backgroundColor = i === 0 ? 'lightgrey' : null
+            const p = document.createElement('p')
+            p.innerText = elements[index]
+            p.style.margin = '3px'
+            index = index + 1
+            td.appendChild(p)
+            tr.appendChild(td)
+        }
+        tbody.appendChild(tr)
+    }
+    table.appendChild(tbody)
+    const p = document.getElementById('firstRowGreyResult')
+    p.appendChild(table)
+    printTitle('h3', 'Alternative table with first row grey HTML code', 'firstRowGreyCodeTitle')
+    const codeContainer = document.getElementById('firstRowGreyHTMLcode')
+    codeContainer.innerText = p.innerHTML
+}
+
+const printTitle = (HTMLtag, text, containerId) =>{
+    const container = document.getElementById(containerId)
+    const title = document.createElement(HTMLtag)
+    title.innerText = text
+    container.appendChild(title)
+}
+
+const innerHTMLCleaner = elementId =>{
+    const element = document.getElementById(elementId)
+    element.innerHTML = ''
+}
+
+const inputCleaner = inputId =>{
+    const input = document.getElementById(inputId)
+    input.value = ''
 }
