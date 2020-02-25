82c736be368a50e30dc21b39310fe5534f9c6a76
MalenaLucero <luceromalena@hotmail.com>
2020-02-24 23:12:06 -0300

    one function to create the three tables

--- main.js
+++ main.js
@@ -1,41 +1,30 @@
+const tables = [
+    {type: 'standard', description: 'Standard table (fixed internal width and margin-left)'},
+    {type: 'alternative', description: 'Alternative table (dynamic internal width and no margin-left)'},
+    {type: 'firstRowGrey', description: 'Alternative table with first row grey'}
+]
 let elements = []
 let rows
 let columns
 
+//main function
 const convert = () =>{
     const input = document.getElementById("textarea").value
-    rows = document.getElementById("rows").value
-    columns = document.getElementById("columns").value
+    rows = parseInt(document.getElementById("rows").value)
+    columns = parseInt(document.getElementById("columns").value)
     if(input !== ''){
-        dataCleaner()
+        hideErrorMessage()
         elements = getElements(input)
         if(checkError()) showErrorMessage()
-        createTable()
-        createAlternativeTable()
-        createFirstRowGreyTable()
+        tables.forEach(table=>createTable(table))
     }
     console.log(elements)
 }
 
-const checkError = () =>
-    error = elements.length === rows * columns ? false : true
+const checkError = () => elements.length === rows * columns ? false : true
 
-const dataCleaner = () =>{
-    innerHTMLCleaner('result')
-    innerHTMLCleaner('HTMLcode')
-    innerHTMLCleaner('alternativeResult')
-    innerHTMLCleaner('alternativeHTMLcode')
-    innerHTMLCleaner('firstRowGreyResult')
-    innerHTMLCleaner('firstRowGreyHTMLcode')
-    innerHTMLCleaner('standardTablePreviewTitle')
-    innerHTMLCleaner('standardTableCodeTitle')
-    innerHTMLCleaner('alternativeTablePreviewTitle')
-    innerHTMLCleaner('alternativeTableCodeTitle')
-    innerHTMLCleaner('firstRowGreyPreviewTitle')
-    innerHTMLCleaner('firstRowGreyCodeTitle')
-    hideErrorMessage()
-}
-
+//creates an array of strings with the user input
+//each string will be a cell in the table
 const getElements = inputValue =>{
     const toHTML = document.createElement('div')
     toHTML.innerText = inputValue
@@ -47,145 +36,88 @@ const getElements = inputValue =>{
     return arrayOfElements
 }
 
-const createTable = () =>{
-    printTitle('h2', 'Preview: Standard table (fixed internal width and margin-left)', 'standardTablePreviewTitle')
-    let index = 0
+//table creation functions
+
+const createTable = tableObject =>{
+    const {type, description} = tableObject
+    dataCleaner(type)
+    printTitle('h2', description, `${type}TablePreviewTitle`)
+    const table = createTableElement()
+    const tbody = document.createElement('tbody')
+    tbody.appendChild(createTableHeader(type))
+    let cellItem = 0
+    for(let i=0; i < rows; i++){
+        const tr = document.createElement('tr')
+        for(let j=0; j < columns; j++){
+            tr.appendChild(createCell(i, j, cellItem, type))
+            cellItem += 1
+        }
+        tbody.appendChild(tr)
+    }
+    table.appendChild(tbody)
+    const tableContainer = document.getElementById(`${type}TablePreview`)
+    tableContainer.appendChild(table)
+    printTitle('h3', 'HTML code:', `${type}TableCodeTitle`)
+    const codeContainer = document.getElementById(`${type}TableCode`)
+    codeContainer.innerText = tableContainer.innerHTML
+}
+
+const createTableElement = () =>{
     const table = document.createElement('table')
     table.cellSpacing = "0"
     table.cellPadding = "0"
     table.border = "1"
-    const tbody = document.createElement('tbody')
-    tbody.border = "1"
+    table.width = '550px'
+    return table
+}
+
+const createTableHeader = (type) =>{
     const specificationsTr = document.createElement('tr')
     const specificationsTd = document.createElement('td')
     specificationsTd.colSpan = `${columns}`
-    specificationsTd.style = "background-color: #cd2e2a; padding-left: 3%; border: 1px solid black"
-    specificationsTd.width = "550px"
+    if(type === 'standard'){
+       specificationsTd.style = "background-color: #cd2e2a; padding-left: 3%; border: 1px solid black" 
+    }else{
+        specificationsTd.style = "background-color: #cd2e2a; padding: 3px; border: 1px solid black" 
+    }
     const specificationsP = document.createElement('p')
     specificationsP.innerText = 'Specifications'
     specificationsP.style = "color: white !important; margin: 0"
     specificationsTd.appendChild(specificationsP)
     specificationsTr.appendChild(specificationsTd)
-    tbody.appendChild(specificationsTr)
-    for(let i=0; i < rows; i++){
-        const tr = document.createElement('tr')
-        tr.border = "1"
-        for(let j=0; j < columns; j++){
-            const td = document.createElement('td')
-            td.style = "padding-left: 3.0%; border: 1.0px solid black"
-            td.width = j === 0 ? "132px" : `${Math.round(418 / columns - 1)}px`
-            const p = document.createElement('p')
-            p.innerText = elements[index]
-            p.style.margin = 0
-            
-            index = index + 1
-            td.appendChild(p)
-            tr.appendChild(td)
-        }
-        tbody.appendChild(tr)
-    }
-    table.appendChild(tbody)
-    const p = document.getElementById('result')
-    p.appendChild(table)
-    printTitle('h3', 'Standard table HTML code:', 'standardTableCodeTitle')
-    const codeContainer = document.getElementById('HTMLcode')
-    codeContainer.innerText = p.innerHTML
+    return specificationsTr
 }
 
-const createAlternativeTable = () =>{
-    printTitle('h2', 'Preview: Alternative table (dynamic internal width and no margin-left)', 'alternativeTablePreviewTitle')
-    let index = 0
-    const table = document.createElement('table')
-    table.width = '550px'
-    table.cellSpacing = "0"
-    table.cellPadding = "0"
-    table.border = "1"
-    const tbody = document.createElement('tbody')
-    tbody.border = "1"
-    const specificationsTr = document.createElement('tr')
-    const specificationsTd = document.createElement('td')
-    specificationsTd.colSpan = `${columns}`
-    specificationsTd.style = "background-color: #cd2e2a; border: 1px solid black"
-    specificationsTd.width = "550px"
-    const specificationsP = document.createElement('p')
-    specificationsP.innerText = 'Specifications'
-    specificationsP.style = "color: white !important; margin: 3px"
-    specificationsTd.appendChild(specificationsP)
-    specificationsTr.appendChild(specificationsTd)
-    tbody.appendChild(specificationsTr)
-    for(let i=0; i < rows; i++){
-        const tr = document.createElement('tr')
-        tr.border = "1"
-        for(let j=0; j < columns; j++){
-            const td = document.createElement('td')
-            td.style = "border: 1.0px solid black"
-            const p = document.createElement('p')
-            p.innerText = elements[index]
-            p.style.margin = '3px'
-            index = index + 1
-            td.appendChild(p)
-            tr.appendChild(td)
-        }
-        tbody.appendChild(tr)
+const createCell = (i, j, cellItemIndex, type) =>{
+    const td = document.createElement('td')
+    if(type === 'standard'){
+        td.style = "padding-left: 3.0%; border: 1.0px solid black"
+        td.width = j === 0 ? "132px" : `${Math.round(418 / columns - 1)}px`
+    }else{
+        td.style = "padding: 3px; border: 1.0px solid black"
     }
-    table.appendChild(tbody)
-    const p = document.getElementById('alternativeResult')
-    p.appendChild(table)
-    printTitle('h3', 'Alternative table HTML code:', 'alternativeTableCodeTitle')
-    const codeContainer = document.getElementById('alternativeHTMLcode')
-    codeContainer.innerText = p.innerHTML
+    td.style.backgroundColor = i === 0 && type === 'firstRowGrey' ? 'lightgrey' : null
+    const p = document.createElement('p')
+    p.innerText = elements[cellItemIndex]
+    p.style.margin = 0
+    td.appendChild(p)
+    return td
 }
 
-const createFirstRowGreyTable = () =>{
-    printTitle('h2', 'Preview: Alternative table with first row grey', 'firstRowGreyPreviewTitle')
-    let index = 0
-    const table = document.createElement('table')
-    table.width = '550px'
-    table.cellSpacing = "0"
-    table.cellPadding = "0"
-    table.border = "1"
-    const tbody = document.createElement('tbody')
-    tbody.border = "1"
-    const specificationsTr = document.createElement('tr')
-    const specificationsTd = document.createElement('td')
-    specificationsTd.colSpan = `${columns}`
-    specificationsTd.style = "background-color: #cd2e2a; border: 1px solid black"
-    specificationsTd.width = "550px"
-    const specificationsP = document.createElement('p')
-    specificationsP.innerText = 'Specifications'
-    specificationsP.style = "color: white !important; margin: 3px"
-    specificationsTd.appendChild(specificationsP)
-    specificationsTr.appendChild(specificationsTd)
-    tbody.appendChild(specificationsTr)
-    for(let i=0; i < rows; i++){
-        const tr = document.createElement('tr')
-        tr.border = "1"
-        for(let j=0; j < columns; j++){
-            const td = document.createElement('td')
-            td.style = "border: 1.0px solid black"
-            td.style.backgroundColor = i === 0 ? 'lightgrey' : null
-            const p = document.createElement('p')
-            p.innerText = elements[index]
-            p.style.margin = '3px'
-            index = index + 1
-            td.appendChild(p)
-            tr.appendChild(td)
-        }
-        tbody.appendChild(tr)
-    }
-    table.appendChild(tbody)
-    const p = document.getElementById('firstRowGreyResult')
-    p.appendChild(table)
-    printTitle('h3', 'Alternative table with first row grey HTML code:', 'firstRowGreyCodeTitle')
-    const codeContainer = document.getElementById('firstRowGreyHTMLcode')
-    codeContainer.innerText = p.innerHTML
-}
+//HTML helpers
 
 const printTitle = (HTMLtag, text, containerId) =>{
     const container = document.getElementById(containerId)
     const title = document.createElement(HTMLtag)
     title.innerText = text
     container.appendChild(title)
+}
+
+const dataCleaner = type =>{
+    innerHTMLCleaner(`${type}TablePreviewTitle`)
+    innerHTMLCleaner(`${type}TablePreview`)
+    innerHTMLCleaner(`${type}TableCodeTitle`)
+    innerHTMLCleaner(`${type}TableCode`)
 }
 
 const innerHTMLCleaner = elementId =>{
